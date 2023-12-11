5814500bef02f0534b91f124e2b9714e34800c86
Vikappa <vincenzo.e.costantini@gmail.com>
2023-12-12 00:47:53 +0100

    Inizio procedura metodo array risposte

--- testlab/testlab.js
+++ testlab/testlab.js
@@ -3,8 +3,9 @@
 
 const apiUrl = 'https://opentdb.com/api.php?amount=50&category=18';
 const body = document.getElementsByName("body")[0]
-let nDomandeFatte = 00
+let nDomandeFatte = 0
 const divTest = document.getElementById("testAppend")
+const arrayRisposte = []
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 
@@ -59,6 +60,46 @@ const generaArrayDomande = async function () {
 
 let arrayDomande = []
 
+async function addRisposta(arrayRispostePresentate, indice_risposta_selezionata, domanda, correct_answer) {
+    let risposta = {}
+    if (arrayRispostePresentate.length > 1) {
+        risposta = {
+            type: `multiple`,
+            question: domanda,
+            answer: arrayRispostePresentate[indice_risposta_selezionata],
+            all_answer: arrayRispostePresentate,
+            correct_answer: correct_answer
+        }
+    } else {
+
+    }
+    arrayRisposte.push(risposta)
+
+    console.log("Lunghezza array risposte: " + arrayRisposte.length)
+    console.log("Array risposte: " + arrayRisposte)
+
+    renderizzaDomande()
+}
+async function addRispostaBool(bool, domanda, correct_answer) {
+    let ans
+    if (bool === "true") {
+        ans = "true"
+    } else {
+        ans = "false"
+    }
+    risposta = {
+        type: `boolean`,
+        question: domanda,
+        answer: ans,
+        all_answer: [`true`, `false`],
+        correct_answer: correct_answer
+    }
+    arrayRisposte.push(risposta)
+    console.log("Lunghezza array risposte: " + arrayRisposte.length)
+    console.log("Array risposte: " + arrayRisposte)
+    renderizzaDomande()
+}
+
 
 
 
@@ -75,16 +116,57 @@ const divDinamicoQuestion = async function (obgDomanda, index) {
     console.log("obg domanda esistente")
     difficulty = obgDomanda.difficulty
     index = index
-    let domanda = obgDomanda.question
-    let risposte = [obgDomanda.correct_answer].concat(obgDomanda.incorrect_answers)
-    const divDomanda = document.createElement("div")
-    const divRisposte = document.createElement("div")
+
     const divRitorno = document.createElement("div")
     const pDomanda = document.createElement("p")
     pDomanda.innerText = obgDomanda.question
     divRitorno.appendChild(pDomanda)
-    console.log("flag")
-    console.log(obgDomanda.question)
+
+    if (obgDomanda.type === `multiple`) {
+        let risposte = [obgDomanda.correct_answer].concat(obgDomanda.incorrect_answers)
+        const divRisposte = document.createElement("div")
+        for (let iRisposte = 0; iRisposte < risposte.length; iRisposte++) {
+            const divRisposta = document.createElement("div")
+            const pRisposta = document.createElement("p")
+            pRisposta.innerText = risposte[iRisposte]
+            pRisposta.classList = "multTypeButton"
+            pRisposta.id = `r` + risposte[iRisposte]
+            pRisposta.onclick = async function () {
+                console.log("click")
+                await addRisposta(risposte, iRisposte, obgDomanda.question, obgDomanda.answer)
+            }
+            divRisposta.appendChild(pRisposta)
+            divRisposte.appendChild(divRisposta)
+        }
+        divRitorno.appendChild(divRisposte)
+
+    }
+    else {
+        const divRispostaBoolean = document.createElement("div")
+        const divVero = document.createElement("div")
+        const divFalso = document.createElement("div")
+        const pVero = document.createElement("p")
+        const pFalso = document.createElement("p")
+        pVero.classList = `booleanButton`
+        pVero.id = `pVero`
+        pVero.onclick = async function () {
+            console.log("click")
+            await addRispostaBool("true", obgDomanda.question, obgDomanda.answer)
+        }
+        pFalso.classList = `booleanButton`
+        pFalso.onclick = async function () {
+            console.log("click")
+            await addRispostaBool("false", obgDomanda.question, obgDomanda.answer)
+        }
+        pFalso.id = `pFalso`
+        pVero.innerText = "True"
+        pFalso.innerText = "False"
+        divFalso.appendChild(pFalso)
+        divVero.appendChild(pVero)
+        divRispostaBoolean.appendChild(divVero)
+        divRispostaBoolean.appendChild(divFalso)
+        divRitorno.appendChild(divRispostaBoolean)
+    }
     return divRitorno
 }
 
@@ -94,10 +176,8 @@ const renderizzaDomande = async function () {
 
     const nuovaDomandaRenderizzata = await divDinamicoQuestion(arrayDomande[nDomandeFatte], nDomandeFatte)
     await divTest.appendChild(nuovaDomandaRenderizzata)
-
 }
 
 renderizzaDomande()
-renderizzaDomande()
-renderizzaDomande()
+
 
