647f06e78ae53c6983034fd41f9f9b3a2bbc90de
Francesco Andrisani <fra.andrisani01@gmail.com>
2023-12-11 16:23:20 +0100

    Creazione della funzione per il grafico a ciambella SENZA CSS

--- scripts/question_page.js
+++ scripts/question_page.js
@@ -1,3 +1,4 @@
+//TIMER
 const timer = function (difficolta) {
   let timer;
   if (difficolta === "easy") {
@@ -20,3 +21,35 @@ const timer = function (difficolta) {
   const intervallo = setInterval(aggiornaTimer, 1000);
 };
 timer("easy");
+
+//GRAFICO
+const graficoCiambella = function (sbagliate, giuste) {
+  const ctx = document.getElementById("graficoCiambella").getContext("2d");
+
+  // Dati del grafico
+  const dati = {
+    datasets: [
+      {
+        data: [sbagliate, giuste], // Valori percentuali per i segmenti del grafico
+        backgroundColor: ["#D20094", "#00FFFF"], // Colori dei segmenti
+        borderColor: "white", // Colore del bordo
+        borderWidth: 2, // Spessore del bordo
+      },
+    ],
+    labels: ["SBAGLIATE", "GIUSTE"], // Etichette dei segmenti
+  };
+
+  // Configurazione del grafico
+  const options = {
+    cutoutPercentage: 30, // Percentuale di spazio vuoto nel centro (30% crea un anello più stretto)
+    responsive: false, // Rendi il grafico non reattivo per evitare ridimensionamenti automatici
+  };
+
+  // Crea il grafico a ciambella
+  const donutChart = new Chart(ctx, {
+    type: "doughnut",
+    data: dati,
+    options: options,
+  });
+};
+graficoCiambella(13, 6);
