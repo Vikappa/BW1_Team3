af2205ab29db92046aca8254db1bc227c34e1e33
Luca Armagno <luca.armagno289@gmail.com>
2023-12-12 17:35:15 +0100

    up

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
-const checkbox = function () {};
+const checkboxChange = function () {
+  let checkbox = document.getElementById('checkbox');
+  let button = document.getElementById('button');
+  button.disabled === !checkbox.checked;
+};
+document.getElementById('checkbox').addEventListener('change', checkboxChange);
+
+const buttonClick = function () {
+  alert('test');
+};
+button = document.getElementById('button');
+button.addEventListener('click', buttonClick);
