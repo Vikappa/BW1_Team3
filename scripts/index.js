7ccc45c119916debe2f7d52ad13d9d1af20f623e
Luca Armagno <luca.armagno289@gmail.com>
2023-12-13 17:48:50 +0100

    checkbox con button

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,22 @@
-// const checkboxChange = function () {
-//   let checkbox = document.getElementById('checkbox');
-//   let button = document.getElementById('button');
-//   button.disabled === !checkbox.checked;
-// };
-// document.getElementById('checkbox').addEventListener('change', checkboxChange);
+const buttonClick = function () {
+  window.open('../questions.html');
+};
+button = document.getElementById('button');
+button.addEventListener('click', buttonClick);
 
-// const buttonClick = function () {
-//   window.open('../questions.html');
-// };
-// button = document.getElementById('button');
-// button.addEventListener('click', buttonClick);
-
-const miaCheckbox = document.getElementById('checkbox');
-let button = document.getElementById('button');
+// const miaCheckbox = document.getElementById('checkbox');
+// const button = document.getElementById('button');
 
 checkbox.addEventListener('click', function () {
   if (checkbox.checked) {
-    button.disabled = false;
+    console.log('true');
+    button.innerHTML = `<button onclick="button" id="buttonProceed">
+    PROCEED
+  </button>`;
   } else {
-   .disabled = true;
+    button.innerHTML = `<button onclick="button" id="buttonProceed" disabled>
+    PROCEED
+  </button>`;
+    console.log('false');
   }
 });
