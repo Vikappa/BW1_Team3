97fd335066eb39ac29b839cdb7c437ee5e3e9c45
Vikappa <vincenzo.e.costantini@gmail.com>
2023-12-14 19:39:09 +0100

    Ricostruito

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+const buttonClick = function () {
+  window.open("../questions.html");
+};
+button = document.getElementById("button");
+button.addEventListener("click", buttonClick);
+
+// const miaCheckbox = document.getElementById('checkbox');
+// const button = document.getElementById('button');
+
+checkbox.addEventListener("click", function () {
+  if (checkbox.checked) {
+    console.log("true");
+    button.innerHTML = `<button onclick="button" id="buttonProceed">
+    PROCEED
+  </button>`;
+  } else {
+    button.innerHTML = `<button onclick="button" id="buttonProceed" disabled>
+    PROCEED
+  </button>`;
+    console.log("false");
+  }
+});
